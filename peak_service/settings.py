a63846409de19538815319fa2a3d134745ea2efd
Daniel Guedegbe <danielguedegbe10027@gmail.com>
2023-03-02 18:37:58 +0000

    ere

--- peak_service/settings.py
+++ peak_service/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         "NAME": "peaks",
         "USER": "daniel",
         "PASSWORD": "10027563",
-        "HOST": "db",
+        "HOST": "127.0.0.1",
         "PORT": "5432",
     }
 }
